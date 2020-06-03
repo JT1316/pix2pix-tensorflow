6e5456083963993a3db821dc29b4d4dd610d7fde
JT1316 <47001022+JT1316@users.noreply.github.com>
2020-06-03 19:16:35 +0100

    print dsicrim loss

--- pix2pix.py
+++ pix2pix.py
@@ -448,6 +448,7 @@ def create_model(inputs, targets):
         # minimizing -tf.log will try to get inputs to 1
         # predict_real => 1
         # predict_fake => 0
+        tf.Print(-(tf.log(predict_real + EPS) + tf.log((1 - predict_fake) + EPS)))
         discrim_loss = (tf.reduce_mean(-(tf.log(predict_real + EPS) + tf.log((1 - predict_fake) + EPS)))) #add bracket
 
     with tf.name_scope("generator_loss"):
