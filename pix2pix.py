3e1056ba792f3a078f5f4ce41cb6658ed1cb479f
JT1316 <47001022+JT1316@users.noreply.github.com>
2020-06-03 19:20:58 +0100

    p update

--- pix2pix.py
+++ pix2pix.py
@@ -448,8 +448,8 @@ def create_model(inputs, targets):
         # minimizing -tf.log will try to get inputs to 1
         # predict_real => 1
         # predict_fake => 0
-        tf.Print(-(tf.log(predict_real + EPS) + tf.log((1 - predict_fake) + EPS)))
-        discrim_loss = (tf.reduce_mean(-(tf.log(predict_real + EPS) + tf.log((1 - predict_fake) + EPS)))) #add bracket
+        pOut = tf.Print(-(tf.log(predict_real + EPS) + tf.log((1 - predict_fake) + EPS)))
+        discrim_loss = (tf.reduce_mean(pOut)) #add bracket
 
     with tf.name_scope("generator_loss"):
         # predict_fake => 1
